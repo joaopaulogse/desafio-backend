a38d4eddc9d8a66ce57820b6ac456a1184b75614
Joao Paulo <joaopaulogse@gmail.com>
2017-07-23 14:44:31 -0300

    preparando pra fazer testes...

---
+++ test/test.js
@@ -0,0 +1,9 @@
+let assert = require("assert");
+
+describe("Pokemon", function() {
+    describe("Criar", function() {
+        it("salvar sem erro", function(done) {
+            //realizar testes de salvar...
+        })
+    })
+})
